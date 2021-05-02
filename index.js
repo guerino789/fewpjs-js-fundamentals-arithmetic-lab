b41d6e8f47a867626670d9f3a174a3f55ebf609f
guerino antonini <guerantonini94@gmail.com>
2021-05-02 14:04:48 -0400

    commit tree e5f2d05af99a8ace90bfde5c73f0c25fe2a1275b

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+const max = Math.max( 5, 10, 15, 20 );
